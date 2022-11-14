e65e7839cfa36558e2c4aa3fa0fe0bad273a087c
friesenb13 <friesenb@uleth.ca>
2022-11-14 11:42:56 -0700

    camera function take pic

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,30 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+    // plugins go here
 
+        var options={
+            quality: 80, //default is 50
+            destinationType: Camera.DestinationType.FILE_URI //this is the default
+        }
+
+        $("#takePhoto").on("click", takePic);
+
+        function takePic(){
+            navigator.camera.getPicture(onSuccess, onError, options)
+        }
+        function onSuccess(imageData){
+            console.log(imageData);
+
+            resolveLocalFileSystemURL(imageData, function (fileEntry) {
+                var myNewImage = fileEntry.toURL()
+                console.log(myNewImage);
+                // do something with URL, assign to src or create an html 
+                $("#takePhoto").after("<img src='"+ myNewImage + " '>")
+            }, onError);
+        }
+
+        function onError(message){
+            alert("Photo not taken because" + message)
+        }
 }
